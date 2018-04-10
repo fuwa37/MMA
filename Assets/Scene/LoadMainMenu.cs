a83997d248af0ca63aacf92bed600b1a2929a116
Bimasakti <bimasaktisinarputra@gmail.com>
2018-04-10 14:02:55 +0700

    Main Menu

---
+++ Assets/Scene/LoadMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadMainMenu : MonoBehaviour {
+	public Button btn1;
+	// Use this for initialization
+	void Start () {
+		Button btn2 = btn1.GetComponent<Button>();
+		btn2.onClick.AddListener(LS);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LS(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
